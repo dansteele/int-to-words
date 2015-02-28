5829654da5072881cfcfcd1daa1d0693d2352f5a
Daniel Steele <danielsteele@hotmail.co.uk>
2015-02-28 14:29:12 +0000

    Added to Fixnum class.

--- int_to_words.rb
+++ int_to_words.rb
@@ -1,3 +1,8 @@
+class Fixnum
+  def to_words
+
 units = ['zero','one','two','three','four','five','six','seven','eight','nine']
 teens = ['eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
 tens = ['ten','twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninety']
+  end
+end
